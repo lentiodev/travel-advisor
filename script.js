3696e46b5d0bf5a1ea050c164fcc8dc1e880dd2e
LENTIO SECHOU <lentiodev@gmail.com>
2023-02-14 16:20:45 +0000

    updated places

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //unsplash api
+/*
       const searchInput = document.getElementById("searchTerm");
       const searchBtn = document.getElementById("searchButton");
       const imagesContainer = document.getElementById("images-container");
@@ -90,109 +91,45 @@
           .catch(error => {
             console.log(error)})})
 
-            
+*/
 
             // google map api
 
-            function initMap(){
+            // Initialize the map
+            var map;
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 37.7749, lng: -122.4194},
+		zoom: 12
+	});
+}
 
-              // Map option
-          
-              var options = {
-                  center: {lat: 38.3460 , lng:-0.4907 },
-                  zoom: 10
-              }
-          
-              //New Map
-              map = new google.maps.Map(document.getElementById("map"),options)
-          
-              //listen for click on map location
-          
-              google.maps.event.addListener(map, "click", (event) => {
-                  //add Marker
-                  addMarker({location:event.latLng});
-              })
-          
-          
-          
-              //Marker
-          /*
-              const marker = new google.maps.Marker({
-              position:{lat: 37.9922, lng: -1.1307},
-              map:map,
-              icon:"https://img.icons8.com/nolan/2x/marker.png"
-              });
-          
-              //InfoWindow
-          
-              const detailWindow = new google.maps.InfoWindow({
-                  content: `<h2>Murcia City</h2>`
-              });
-          
-              marker.addListener("mouseover", () =>{
-                  detailWindow.open(map, marker);
-              })
-              */
-          
-              //Add Markers to Array
-          
-              let MarkerArray = [ {location:{lat: 37.9922, lng: -1.1307}, 
-                  imageIcon: "https://img.icons8.com/nolan/2x/marker.png", 
-                  content: `<h2>Murcia City</h2>`},
-          
-                  {location:{lat: 39.4699, lng: -0.3763}},
-          
-                  {location:{lat: 38.5411, lng: -0.1225},content: `<h2>Benidorm City</h2>` }
-          
-          
-          
-              ]
-          
-              // loop through marker
-              for (let i = 0; i < MarkerArray.length; i++){
-                  addMarker(MarkerArray[i]);
-          
-              }
-          
-              // Add Marker
-          
-              function addMarker(property){
-          
-                  const marker = new google.maps.Marker({
-                      position:property.location,
-                      map:map,
-                      //icon: property.imageIcon
-                      });
-          
-                      // Check for custom Icon
-          
-                      if(property.imageIcon){
-                          // set image icon
-                          marker.setIcon(property.imageIcon)
-                      }
-          
-                      if(property.content){
-          
-                      const detailWindow = new google.maps.InfoWindow({
-                      content: property.content
-              });
-              
-              marker.addListener("mouseover", () =>{
-                  detailWindow.open(map, marker);
-              })
-          }
-          
-                   
-          
-          
-          
-              }
-          
-              
-          
-          
-          
-          }
+// Search for places
+function search() {
+	var searchInput = document.getElementById('search-input').value;
+	var request = {
+		query: searchInput,
+		fields: ['name', 'geometry']
+	};
+	var service = new google.maps.places.PlacesService(map);
+	service.textSearch(request, function(results, status) {
+		if (status === google.maps.places.PlacesServiceStatus.OK) {
+			for (var i = 0; i < results.length; i++) {
+				createMarker(results[i]);
+			}
+			map.setCenter(results[0].geometry.location);
+		}
+	});
+}
+
+// Create a marker for each place
+function createMarker(place) {
+	var marker = new google.maps.Marker({
+		map: map,
+		position: place.geometry.location
+	});
+}
+
 
 
 
